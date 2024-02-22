26a46ecf116945451a84bb2fcec1d02036a20034
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2024-02-22 15:44:55 +0100

    Created enemypool that stores enemies (max 20), spawns 20, sets them as false and activates them when they get called to be spawned i guess

--- Assets/Scripts/P1_Pooling/EnemySpawner.cs
+++ Assets/Scripts/P1_Pooling/EnemySpawner.cs
@@ -3,12 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using UnityEngine.Pool;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private IObjectPool<Enemy> _enemyPool;
     public Enemy EnemyPrefab;
     private const float _totalCooldown = 2f;
+    [SerializeField] private bool collectionCheck = true;
+    [SerializeField] private int defaultCapacity = 20;
+    [SerializeField] private int maxSize = 100;
+    
     private float _currentCooldown;
+
+    private void Awake()
+    {
+        _enemyPool = new ObjectPool<Enemy>(CreateFunc, ActionOnGet, ActionOnRelease, ActionOnDestroy,
+            collectionCheck, defaultCapacity, maxSize);
+        for (int i = 0; i < 20; i++)
+        {
+            _enemyPool.Release(CreateFunc());
+        }
+    }
+
+    private void ActionOnDestroy(Enemy obj)
+    {
+        Destroy(obj.gameObject);
+    }
+
+    private void ActionOnRelease(Enemy obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+
+    private void ActionOnGet(Enemy obj)
+    {
+        obj.gameObject.SetActive(true);
+    }
+
+    private Enemy CreateFunc()
+    {
+        Enemy fiende = Instantiate(Enemy);
+        fiende.gameObject.SetActive(false);
+        fiende.ObjectPool = _enemyPool;
+        return fiende;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
